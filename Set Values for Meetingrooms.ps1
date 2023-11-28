5fc42cdda43216e2eca29008adefcd79e0946350
aha345 <134587220+aha345@users.noreply.github.com>
2023-11-28 11:57:15 +0100

    Re-wrote the whole thing
    Fixed a lot of things

--- Set Values for Meetingrooms.ps1
+++ Set Values for Meetingrooms.ps1
@@ -1,22 +1,104 @@
-# Check if AzureAD credentials are already stored
-if (-not $Credential) {
-    # Prompt the user for AzureAD credentials
-    $Credential = Get-Credential -Message "Enter your AzureAD credentials"
+# Connect to Exchange Online with the provided credentials
+try {
+    Get-AcceptedDomain -ErrorAction Stop -WarningAction SilentlyContinue | Out-Null
+} catch {
+     if {
+        if (-not $Credential) {
+            # Prompt the user for AzureAD credentials
+            $Credential = Get-Credential -Message "Enter your AzureAD credentials"
+        }
+
+        try {
+            Connect-ExchangeOnline -Credential $Credential -ErrorAction Stop | Out-Null
+        } catch {
+            $ConditionalAccess = $True
+        }
+    } else ($ConditionalAccess) {
+        Connect-ExchangeOnline | Out-Null
+    }
 }
 
-# Connect to Exchange Online with the provided credentials
-Connect-ExchangeOnline -Credential $Credential -ShowBanner:$false
+#-----------------------------------------------------------------------------#
+
+# Change this to specify
+$AddOrganizerToSubject = $true
+$RemovePrivateProperty = $false
+$DeleteSubject = $false
+$DeleteComments = $true
+
+#-----------------------------------------------------------------------------#
+
+if ($AddOrganizerToSubject) {
+    $AddOrganizerToSubjectvalue = "True"
+} else {
+    $AddOrganizerToSubjectvalue = "False"
+}
+
+if ($RemovePrivateProperty) {
+    $RemovePrivatePropertyvalue = "True"
+} else {
+    $RemovePrivatePropertyvalue = "False"
+}
+
+if ($DeleteSubject) {
+    $DeleteSubjectvalue = "True"
+} else {
+    $DeleteSubjectvalue = "False"
+}
+
+if ($DeleteComments) {
+    $DeleteCommentsvalue = "True"
+} else {
+    $DeleteCommentsvalue = "False"
+}
 
 # Blank list for changed meeting rooms
 $Changed = $null
 
-# Get Room mailboxes
-$Rooms = Get-Mailbox -RecipientTypeDetails RoomMailbox
+# Set success to false
+$Success = $false
+
+while (-not $Success) {
+    try {
+        # Get name of mailbox
+        $RoomName = $(Write-Host "What is the UPN of the room? " -NoNewLine) + $(Write-Host "(Type 'All' for all rooms or specify UPN) : " -ForegroundColor Yellow -NoNewline; Read-Host)
+
+        if ($RoomName -ne "All") {
+            # Get mailbox info
+            $Rooms = Get-MailBox -Identity $RoomName -RecipientTypeDetails RoomMailbox -ErrorAction Stop
+
+            # If found set to True and give info
+            Write-Host "Room found"
+            $Success = $True
+        } elseif ($RoomName -eq "All") {
+            # Get Room mailboxes
+            $Rooms = Get-Mailbox -RecipientTypeDetails RoomMailbox
+            $Success = $True
+        }
+    } catch {
+        Write-Warning "Room not found, try again!"
+        Start-Sleep -Seconds 3
+    }
+}
+
+$Changed = @()
 
 #Loop each room in $Rooms
 foreach ($Room in $Rooms) {
 
     $success = $false
+
+    try {
+
+        $CalendarName=($CalendarFolder=Get-MailboxFolderStatistics -Identity $Room.PrimarySMTPAddress -FolderScope Calendar | Where-Object {$_.FolderType -eq 'Calendar'}|Select-Object Name).Name
+
+        Set-MailBoxFolderPermission -Identity "$($Room.PrimarySMTPAddress):\$($CalendarName)" -User "Default" -AccessRights "Reviewer"
+
+    } catch {
+
+        Write-Warning "Could not set permission for meetingroom"
+
+    }
 
     while ($success -ne $true) {
         
@@ -28,19 +110,19 @@ foreach ($Room in $Rooms) {
         # Output Values
         Write-Host ""
         Write-Host "What is the current values:" -ForegroundColor Cyan
-        Write-Host "    AddOrganizerToSubject: $($RoomValue.AddOrganizerToSubject) (Should be 'True')" -ForegroundColor Gray
-        Write-Host "    RemovePrivateProperty: $($RoomValue.RemovePrivateProperty) (Should be 'True')" -ForegroundColor Gray
-        Write-Host "    DeleteSubject: $($RoomValue.DeleteSubject) (Should be 'False')" -ForegroundColor Gray
-        Write-Host "    DeleteComments: $($RoomValue.DeleteComments) (Should be 'True')" -ForegroundColor Gray
+        Write-Host "    AddOrganizerToSubject: $($RoomValue.AddOrganizerToSubject) (Should be $AddOrganizerToSubjectvalue)" -ForegroundColor Gray
+        Write-Host "    RemovePrivateProperty: $($RoomValue.RemovePrivateProperty) (Should be $RemovePrivatePropertyvalue)" -ForegroundColor Gray
+        Write-Host "    DeleteSubject: $($RoomValue.DeleteSubject) (Should be $DeleteSubjectvalue)" -ForegroundColor Gray
+        Write-Host "    DeleteComments: $($RoomValue.DeleteComments) (Should be $DeleteCommentsvalue)" -ForegroundColor Gray
         Write-Host ""
 
         try{
 
-            if (($RoomValue.AddOrganizerToSubject -ne $true) -or ($RoomValue.RemovePrivateProperty -ne $true) -or ($RoomValue.DeleteSubject -ne $false) -or ($RoomValue.DeleteComments -ne $True)) {
+            if (($RoomValue.AddOrganizerToSubject -ne $AddOrganizerToSubject) -or ($RoomValue.RemovePrivateProperty -ne $RemovePrivateProperty) -or ($RoomValue.DeleteSubject -ne $DeleteSubject) -or ($RoomValue.DeleteComments -ne $DeleteComments)) {
 
                 try {
 
-                    Set-CalendarProcessing -Identity $Room.UserPrincipalName -AddOrganizerToSubject $true -RemovePrivateProperty $true -DeleteSubject $false -DeleteComments $true -ErrorAction Stop
+                    Set-CalendarProcessing -Identity $Room.UserPrincipalName -AddOrganizerToSubject $AddOrganizerToSubject -RemovePrivateProperty $RemovePrivateProperty -DeleteSubject $DeleteSubject -DeleteComments $DeleteComments -ErrorAction Stop
 
                     Write-Host "Values set for $($Room.UserPrincipalName)" -ForegroundColor Yellow
 
@@ -82,14 +164,9 @@ foreach ($Room in $Rooms) {
 }
 
 if ($Changed -ne $null) {
-    
     Write-Host ""
     Write-Host "These rooms have been changed:" -ForegroundColor Yellow    
-    
     foreach ($Change in $Changed) {
-        
         Write-Host "    - $($Change)" -ForegroundColor Cyan
-    
     }
-
 }
